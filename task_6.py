1e8c980818a52821c14d8f326d1719eac9623173
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2019-03-29 10:25:24 +0200

    task_6 changeg

--- task_6.py
+++ task_6.py
@@ -7,34 +7,17 @@ def is_valid(p):
     if pl.Path(p).is_file():
         return True
 
-def file_processing(f, mode):
-        s = f.readlines()
-        s = ''.join(s[1:]).split('\n')
-        if mode == "Moskow":
-                k = 0
-                for numb in s:
-                    first_sum = 0
-                    last_sum = 0
-                    for symbol in numb[:3]:
-                        first_sum += int(symbol)
-                    for symbol in numb[3:]:
-                        last_sum += int(symbol)
-                    if first_sum == last_sum:
-                        k+=1
-        else:
-            k = 0
-            for numb in s:
-                even_sum = 0
-                odd_sum = 0
-                for i in range(0, 6):
-                    if i % 2 == 0:
-                        even_sum += int(numb[i])
-                    else:
-                        odd_sum += int(numb[i])
 
-                if even_sum == odd_sum:
-                    k += 1
-        f.close()
+def file_processing(f, mode):
+        k = 0
+        for i in range(1, 1000000):
+            ticket = '{:06}'.format(i)
+            if mode == "Moskow":
+                if sum(map(int, ticket[:3])) == sum(map(int, ticket[3:])):
+                        k += 1
+            else:
+                if sum(map(int, ticket[0::2])) == sum(map(int, ticket[1::2])):
+                        k += 1
         print(f'Happy tickets for the method "{mode}" appear in file {k} time(s).')
 
 
@@ -59,7 +42,7 @@ if __name__ == '__main__':
         mode = str(f.readline().strip())
         if mode == "Moskow" or mode == "Piter":
             file_processing(f, mode)
-        else :
+        else:
             print("Wrong mode.")
     else:
         interactive()
