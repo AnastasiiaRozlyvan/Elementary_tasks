5e851ec66616bdd69d5f3113e4d6a00e5495940a
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2019-04-01 20:51:27 +0300

    tests changed

---
+++ tests/test_task_8.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest import TestCase
+import task_8 as t
+
+
+class TestFib(TestCase):
+    def test_params_assigned_0(self):
+        self.assertFalse(t.params_assigned(0, 0))
+
+    def test_params_assigned_negative(self):
+        self.assertFalse(t.params_assigned(-55, -19))
+
+    def test_params_assigned_str(self):
+        self.assertFalse(t.params_assigned("abc", "abc"))
+
+    def test_params_assigned(self):
+        self.assertTrue(t.params_assigned(17, 60))
+
+
+if __name__ == '__main__':
+    unittest.main()
